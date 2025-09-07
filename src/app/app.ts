1e5d7134754c25d64f75ca70390cda6f90dbdb88
ethanmcf <ethan_mcfarland@outlook.com>
2025-09-07 14:18:55 -0400

    Created template pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { About } from './components/about/about';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, About, Experience, Projects, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
