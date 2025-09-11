761a4a6658de55d7208789b66cd8ad82197011cb
ethanmcf <ethan_mcfarland@outlook.com>
2025-09-11 15:48:34 -0400

    Updated experience content

--- src/app/components/experience/experience.ts
+++ src/app/components/experience/experience.ts
@@ -14,30 +14,42 @@ import { TimelineData } from '../../models/timeline.model';
 export class ExperienceComponent {
   experiences: TimelineData[] = [
     {
+      title: 'DevOps Specalist Intern',
+      company: 'Tangerine',
+      url: 'https://www.tangerine.ca',
+      period: 'Summer 2025',
+      location: 'Toronto, CA',
+      details: [
+        'Automated code deployments by building CI/CD pipelines in Jenkins and Bash, reducing manual integration branch deployments by 30% and improving deployment consistency',
+        'Scaled and managed 20+ Kubernetes microservices across 5 development environments, ensuring stable deployments and handling increased traffic'
+      ]
+    },
+    {
+      title: 'Software Developer Intern',
+      company: 'Socitabank',
+      url: 'https://www.scotiabank.com/ca/en/personal.html',
+      period: 'Summer 2024',
+      location: 'Toronto, CA',
+      details: [
+        'Designed and implemented a custom ServiceNow form to streamline ticket processing by auto-filling employee information, reducing manual input by 40% and improving data consistency for the internal development team',
+        'Developed full-stack applications, utilizing JavaScript for front-end development and integrating REST APIs with automated workflows on the ServiceNow platform'
+      ]
+    },
+    {
       title: 'Quality Engineer Intern',
       company: 'Tangerine',
       url: 'https://www.tangerine.ca',
       period: 'Summer 2023',
       location: 'Toronto, CA',
       details: [
-        'Contributed to a regression team, ensuring software consistency post-updates for mobile and web applications, emphasizing quality and stability',
-        'Collaborated closely with developers and QA to refine testing processes'
+        "Restructured Tangerine's Selenium WebDriver implementation, enhancing the speed of mobile web testing execution by over 25%, with a focus on supporting offshore teams and remote employees",
+        'Developed Java and TypeScript scripts to help developers expand front-end and api test coverage by 10%'
       ]
     },
     {
       title: 'Web Designer',
       company: 'TileCo Inc',
-      url: '#',
-      period: '2021–2022',
-      location: 'Toronto, CA',
-      details: [
-        'Engaged in the development of the design, encompassing responsibilities including the management of image updates and the introduction of new products'
-      ]
-    },
-    {
-      title: 'Web Designer',
-      company: 'TileCo Inc',
-      url: '#',
+      url: 'https://tilecoinc.ca/',
       period: '2021–2022',
       location: 'Toronto, CA',
       details: [
